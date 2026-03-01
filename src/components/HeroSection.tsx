e8887fa3fe82c2297cf49a1d444e9f0ac278a228
RamOfFate <ramoffate@gmail.com>
2026-03-01 19:23:03 +0100

    logo stuff

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
-import PixelSword from '@/../public/pixel sword.jpg'
-import Editor from '@/../public/editor.png'
+import PixelSword from '/public/pixel sword.jpg'
+import Editor from '/public/editor.png'
 
 export default function HeroSection() {
   return (
@@ -9,16 +9,15 @@ export default function HeroSection() {
       </div>
       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full pointer-events-none bg-linear-to-t from-background from-20% to-50% to-emerald-300/0 z-20 "></div>
 
-      <div className="relative z-30 text-center px-6 max-w-5xl">
-        <h1 className="text-5xl md:text-7xl mb-6 tracking-tight bg-linear-to-b from-foreground to-foreground/50 bg-clip-text text-transparent">
-          LOCK IN_ <br />
-          <span className="text-primary">By Dusk&Dawn.</span>
-        </h1>
-
+      <div className="relative z-30 text-center px-6 max-w-5xl space-y-4 pt-16">
         <p className="text-foreground text-lg md:text-xl max-w-2xl mx-auto ">
           Stop gaming. Start building. The competitive arena for programmers who
           lack discipline but love the grind.
         </p>
+
+        <h1 className="text-5xl md:text-7xl mb-6 tracking-tight bg-linear-to-b from-foreground to-foreground/50 bg-clip-text text-transparent">
+          LOCK IN<span className="text-primary animate-peekaboo">_</span> <br />
+        </h1>
       </div>
 
       <div className="relative rounded-2xl  mx-32 shadow-2xl z-10 mt-20 border-2 overflow-hidden">
