4ef3a029b4d4ad060a6db94b7fdceea4c5579901
SlimBizid <slimbizid879@gmail.com>
2026-04-07 17:51:14 +0100

    ts fix

--- src/routes/profile/edit/index.tsx
+++ src/routes/profile/edit/index.tsx
@@ -90,7 +90,7 @@ export function EditProfilePage() {
             <p className="mb-1">Profile updated successfully!</p>
             <p className="mb-0">
               <Link
-                to={`/profile/${user.username}`}
+                to={`/profile/${user?.username}`}
                 className="font-semibold text-emerald-700 hover:text-emerald-900"
               >
                 Go back to your profile
